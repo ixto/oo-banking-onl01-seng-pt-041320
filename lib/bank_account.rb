92975a9a3565cd1d2a731d52e12724a32364c45a
Joey <joey.alzamora@gmail.com>
2020-05-19 00:36:22 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
-
+ attr_accessor :bank_account, :balance, :status  
+ attr_reader :name 
+ 
+ def initialize(name)
+   @name = name 
+   @bank_account = bank_account
+   @balance = 1000 
+   @status = 'open' 
+ end 
+ 
+ def deposit
+   
+ end 
+ 
 end
